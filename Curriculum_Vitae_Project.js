9430f4fdcd2dae3a6d253d99840747a7e64a2cdc
Deathvanos <char.mailley@gmail.com>
2025-06-13 09:48:48 +0200

    Fix @media print

--- Curriculum_Vitae_Project.js
+++ Curriculum_Vitae_Project.js
@@ -1,27 +1,26 @@
 
 
 /*  On page load, scan all pages for manage overrun */
-document.addEventListener("DOMContentLoaded", function() {
-    const countLoopError = 0;
+document.addEventListener("DOMContentLoaded", function () {
     const sheets = document.querySelectorAll('.sheet');
     sheets.forEach(sheet => {
-        manageOverrun(sheet)
+        manageOverrun(sheet, 0);
     });
 });
 /** 
  * Check that no text is overflowing at the bottom of the page. If so, move the excess content to a new page. 
  * @param {HTMLElement} sheet - the sheet to check.
 */
-    function manageOverrun(sheet) {
+function manageOverrun(sheet, countLoopError) {
     const newSheet = document.createElement('section');
     newSheet.classList.add('sheet', 'padding');
+
     while (sheet.scrollHeight > sheet.clientHeight) {
         newSheet.insertBefore(sheet.lastElementChild, newSheet.firstChild);
         sheet.parentNode.insertBefore(newSheet, sheet.nextSibling);
     }
+
     if (newSheet.scrollHeight > newSheet.clientHeight && countLoopError < 50) {
-        manageOverrun(newSheet);
-        countLoopError++;
-        const countLoopError = 0;
+        manageOverrun(newSheet, countLoopError + 1);
     }
 }
